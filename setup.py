b662b2230c079ece93d0768b801739afe2f04b04
halfak <aaron.halfaker@gmail.com>
2015-09-14 17:10:18 -0500

    Increments version to 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def requirements(fname):
 
 setup(
     name="para",
-    version="0.0.3",  # update in para/__init__.py too
+    version="0.0.4",  # update in para/__init__.py too
     author="Aaron Halfaker",
     author_email="aaron.halfaker@gmail.com",
     packages=find_packages(),
