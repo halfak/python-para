b6e336c1f5b7c26358a11f2dcd6cc42475e2673b
halfak <aaron.halfaker@gmail.com>
2015-09-14 16:47:15 -0500

    Adds a special case for single-item processing in map()

--- para/tests/test_map.py
+++ para/tests/test_map.py
@@ -18,3 +18,18 @@ def test_map():
             ("bar", 1),
             ("baz", 1)
         })
+
+def test_map_single():
+    words = ["baz"]
+
+    def get_the_a(word):
+        for i, char in enumerate(word):
+            if char == 'a':
+                yield word, i
+
+    set_of_a = set(map(get_the_a, words))
+
+    eq_(set_of_a,
+        {
+            ("baz", 1)
+        })
